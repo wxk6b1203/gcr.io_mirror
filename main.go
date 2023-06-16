83d83d9d7e0cabd5b6beff277dfe17ef7bfdc91b
wuxk <wuxk@chinatelecom.cn>
2023-06-16 19:12:38 +0800

    (fix) 2 remove version

--- main.go
+++ main.go
@@ -242,7 +242,7 @@ func mirrorByIssues(issues *github.Issue, config *Config) (err error, originImag
 }
 
 func dockerLogin(config *Config) (*client.Client, context.Context, error) {
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithVersion(""))
+	cli, err := client.NewClientWithOpts(client.WithVersion(""))
 	if err != nil {
 		return nil, nil, err
 	}
